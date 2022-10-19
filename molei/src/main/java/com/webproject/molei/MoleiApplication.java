e2b9a4d3dcfe8aaeaca61beb4ac7d55e1666c2c2
정밀지도송출개발팀/홍성표 <sungpyo.hong@hyundai-autoever.com>
2022-10-19 15:10:30 +0900

    [홍성표, MCLOS-0, NONE][제목] 프로젝트 재생성
    [원인]
    -
    
    [개선내역]
    -
    
    [영향도]
    1. 사이드이펙트 가능성: 없음
    2. 타플랫폼 영향성: 없음
    3. 타향지 영향성 : 없음
    
    [변경점 체크리스트]
    -

---
+++ molei/src/main/java/com/webproject/molei/MoleiApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.molei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoleiApplication.class, args);
+	}
+
+}
---
+++ molei/src/test/java/com/webproject/molei/MoleiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.molei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoleiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
